c14d6dbfde78082c5253efd1f0f53ae47d82b964
adnanhd <adnanharundogan@gmail.com>
2020-07-22 23:09:45 +0300

    revise project

--- hectorquad/build/CMakeFiles/hectorquad_generate_messages_cpp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/hectorquad_generate_messages_cpp"
-  "devel/include/hectorquad/coordinate.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/hectorquad_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- hectorquad/build/devel/share/hectorquad/cmake/hectorquad-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(hectorquad_MESSAGE_FILES "")
-set(hectorquad_SERVICE_FILES "/home/adnanhd/swarm/src/hectorquad/srv/coordinate.srv")
