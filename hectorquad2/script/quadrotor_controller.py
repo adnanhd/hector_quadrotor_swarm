7408d15f8384133525a86389a3ef6e3bb88e778f
adnanhd <adnanharundogan@gmail.com>
2020-08-09 12:26:02 +0300

    update quadrotor_controller.py: optimize proximal control

--- hectorquad2/script/quadrotor_controller.py
+++ hectorquad2/script/quadrotor_controller.py
@@ -11,34 +11,43 @@ msg_pose = PoseStamped()
 
 def get_position(msg):
     global msg_pose
-    msg_pose = copy.deepcopy(msg)
+    msg_pose = msg
 
 
 def set_position(msg):
     global msg_twist
     global msg_pose
-    repulsive_constant = 5.0
-    attractive_constant = 10.0
+
+    o_des = 15.0
+    C = 225.0
+
     x = msg_pose.pose.position.x - msg.pose.position.x
     y = msg_pose.pose.position.y - msg.pose.position.y
     z = msg_pose.pose.position.z - msg.pose.position.z
+    msg_twist[3] += 1
 
-    rospy.logerr_throttle(5.0, '%s <--> %s %3.2f x=%2.1f y=%2.1f z=%2.1f' % (
-        msg_pose.header.frame_id, msg.header.frame_id, math.sqrt(x*x + y*y + z*z), x, y, z))
-    if (repulsive_constant > math.sqrt(x*x + y*y + z*z)):
-        msg_twist[0] += x
-        msg_twist[1] += y
-        msg_twist[2] += z
-        msg_twist[3] += 1
-    elif (attractive_constant < math.sqrt(x*x + y*y + z*z)):
-        msg_twist[0] -= x
-        msg_twist[1] -= y
-        msg_twist[2] -= z
-        msg_twist[3] += 1
+    r = math.sqrt(x * x + y * y + z * z)
+
+    rospy.loginfo_throttle(1.0, '%s--%s x:%2.1f y:%2.1f z:%2.1f' %
+                           (rospy.get_namespace(), 'none', x, y, z))
+
+    if (20 < r or r < 10):
+        if x > o_des:
+            msg_twist[0] -= (x-o_des)**2/C
+        else:
+            msg_twist[0] += (x-o_des)**2/C
+        if y > o_des:
+            msg_twist[1] -= (y-o_des)**2/C
+        else:
+            msg_twist[1] += (y-o_des)**2/C
+        if z > o_des:
+            msg_twist[2] -= (z-o_des)**2/C
+        else:
+            msg_twist[2] += (z-o_des)**2/C
 
 
 if __name__ == '__main__':
-    num_of_drones = len(rospy.myargv()) - 1
+    num_of_drones = len(rospy.myargv())
     node_name = '' if num_of_drones == 0 else '/' + rospy.myargv()[1]
 
     rospy.init_node('driver', anonymous=True)
@@ -52,7 +61,7 @@ if __name__ == '__main__':
 
     pub = rospy.Publisher(node_name + '/cmd_vel', Twist, queue_size=10)
 
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(5)
     msg = Twist()
 
     while not rospy.is_shutdown():
@@ -61,7 +70,9 @@ if __name__ == '__main__':
         msg.linear.z = msg_twist[2] / msg_twist[3]
 
         pub.publish(msg)
-        rospy.loginfo_throttle(5.0, '%s: %2.1f %2.1f %2.1f' % (
-            rospy.get_namespace(), msg.linear.x, msg.linear.y, msg.linear.z))
-        msg_twist = [0, 0, 0, 1.0]
+
+        msg_twist[0] = 0
+        msg_twist[1] = 0
+        msg_twist[2] = 0
+        msg_twist[3] = 1.0
         rate.sleep()
