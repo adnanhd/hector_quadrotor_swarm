d5d84daa68905f141395474760f4249efaf8883f
adnanhd <adnanharundogan@gmail.com>
2020-08-15 16:55:10 +0300

    update slave_test.py

--- hectorquad2/script/focal_test.py
+++ hectorquad2/script/focal_test.py
@@ -5,26 +5,31 @@ import math
 from geometry_msgs.msg import Twist, Quaternion, PoseStamped
 
 
-def quaternionToAngle(quad):
-    # Transform the quad of the form PoseStamped().pose.orientation to angle
-    # by applying atan2(y,x) => float angle from math library.
-    return math.atan2(2.0*(quad.y * quad.z + quad.w * quad.x), quad.w * quad.w - quad.x * quad.x - quad.y * quad.y + quad.z * quad.z)
-
-
 def update_position(msg, position):
     position[0] = msg.pose.position.x
     position[1] = msg.pose.position.y
     position[2] = msg.pose.position.z
+    position[3] = math.atan2(2.0 * (msg.pose.orientation.w * msg.pose.orientation.z + msg.pose.orientation.x * msg.pose.orientation.y),
+                             1.0 - 2.0 * (msg.pose.orientation.y * msg.pose.orientation.y + msg.pose.orientation.z * msg.pose.orientation.z))
 
-    angle = quaternionToAngle(msg.pose.orientation)
-    rospy.logerr_throttle(4.0, 'x:%2.1f y:%2.1f z:%2.1f' % (
-        msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z))
+
+def coeff(a, b):
+    if abs(a-b) < 1:
+        return 0
+    elif(a < b):
+        return 1
+    else:
+        return -1
+
+
+def PoseToStr(pose):
+    return '[x:%2.1f y:%2.1f z:%2.1f][x:%2.1f y:%2.1f z:%2.1f]' % (pose.pose.position.x, pose.pose.position.y, pose.pose.position.z, pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z)
 
 
 if __name__ == '__main__':
     waypoints = [(10, 10, 25), (10, -10, 25), (-10, -10, 25), (-10, 10, 25)]
-    position = [0, 0, 0]
-    velocity = [0, 0, 0]
+    position = [0, 0, 0, 0]
+    velocity = [0, 0, 0, 0]
     msg = Twist()
     i = 1
 
@@ -35,22 +40,21 @@ if __name__ == '__main__':
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     rate = rospy.Rate(10)
 
-    rospy.spin()
-
     while not rospy.is_shutdown():
-        
+
         for dim in [0, 1, 2]:
             if abs(waypoints[i][dim] - position[dim]) > 0.25:
                 velocity[dim] = (waypoints[i][dim] - position[dim]) / 2.0
             else:
                 velocity[dim] = 0
 
-        if (velocity == [0, 0, 0]):
+        if (velocity[0:3] == [0, 0, 0]):
             i = (i+1) % len(waypoints)
 
         msg.linear.x = velocity[0]
         msg.linear.y = velocity[1]
         msg.linear.z = velocity[2]
+        msg.angular.z = velocity[3]
 
         pub.publish(msg)
 
--- hectorquad2/script/slave_test.py
+++ hectorquad2/script/slave_test.py
@@ -1,204 +1,155 @@
 #!/usr/bin/env python
-
 import sys
 import rospy
 import math
 import copy
 from geometry_msgs.msg import Twist, Quaternion, PoseStamped
-epsion = 0.001
 
+allignment_data = []
+msg_twist = [0, 0, 0, 1.0]
+msg_pose = PoseStamped()
 
-def slave_callback(msg, pose):
-    """
-    Documentation for a function.
+def heading_allignment2(loc):
+	global allignment_data
+	#rospy.loginfo(str(loc.pose.position.x))
+	flag = False
+	for data in allignment_data:
+		if data[0] == 2:
+			data[1] = loc
+			flag = True
+	if flag == False:
+		allignment_data.append([2, loc])
+'''
+def heading_allignment3(loc):
+	global allignment_data
+	flag = False
+	for data in allignment_data:
+		if data[0] == 3:
+			data[1] = loc
+			flag = True
+	if flag == False:
+		allignment_data.append([3, loc])
 
-    More details.
-    """
-    # Update the agent's position as soon as the topic
-    # ground_truth_to_tf/pose published
-    pose = msg
+def heading_allignment4(loc):
+	global allignment_data
+	flag = False
+	for data in allignment_data:
+		if data[0] == 4:
+			data[1] = loc
+			flag = True
+	if flag == False:
+		allignment_data.append([4, loc])
 
+def heading_allignment5(loc):
+	global allignment_data
+	flag = False
+	for data in allignment_data:
+		if data[0] == 5:
+			data[1] = loc
+			flag = True
+	if flag == False:
+		allignment_data.append([5, loc])'''
 
-def agent_callback(msg, args):
-    """
-    Documentation for a function.
-
-    More details.
-    """
-    # initializing agents index and updating its position in the list
-    swarm_pose = args[0]
-    agent_index = args[1]
-    swarm_pose[agent_index] = msg
-
-
-def quaternionToAngle(quad):
-    """
-    Documentation for a function.
-
-    More details.
-    @param quad is a value of the form PoseStamped().pose.orientation 
-    @return angle
-    """
-    # Transform the quad of the form PoseStamped().pose.orientation to angle
-    # by applying atan2(y,x) => float angle from math library.
-    return math.atan2(2.0*(quad.y * quad.z + quad.w * quad.x), quad.w * quad.w - quad.x * quad.x - quad.y * quad.y + quad.z * quad.z)
-
-
-def TwistToStr(twist):
-    return '[x:%2.1f y:%2.1f z:%2.1f][x:%2.1f y:%2.1f z:%2.1f]' % (twist.linear.x, twist.linear.y, twist.linear.z, twist.angular.x, twist.angular.y, twist.angular.z)
-
-
-def PoseToStr(pose):
-    return '[x:%2.1f y:%2.1f z:%2.1f][x:%2.1f y:%2.1f z:%2.1f]' % (pose.pose.position.x, pose.pose.position.y, pose.pose.position.z, pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z)
-
+def focal_control(data):
+	global msg_pose
+	msg_pose = data
 
 if __name__ == '__main__':
-    swarm = rospy.myargv()[1:]
-    # initialize swarm_pose with default PoseStamped() objects
-    # in order to keep position information of the agents in the swarm 
-    swarm_pose = [PoseStamped()] * len(swarm)
-    # initialize slave_pose with a PoseStamped() object to keep postion information 
-    # of the slave in the swarm
-    slave_pose = PoseStamped()
-    # initialize slave_vel with a Twist() object to update velocity information
-    # of the slave
-    slave_vel = Twist()
-    z_yaw = 0  # not used
 
-    # Initialize node to controll the agent
-    rospy.init_node('slave', anonymous=True)
+	rospy.init_node('driver', anonymous=True)	
+	node_name = rospy.myargv()[1]
 
-    # Subscribe the position of the agent and the rest of agents in the swarm
-    rospy.Subscriber('ground_truth_to_tf/pose',
-                     PoseStamped, slave_callback, slave_pose)
+	rospy.Subscriber('ground_truth_to_tf/pose', PoseStamped, focal_control)
 
-    # Subscribe pose topic of the agents in the swarm to keep swarm_pose up-to-date
-    # i.e., keep all agents' position in the swarm
-    for agent_index in range(len(swarm)):
-        rospy.Subscriber('/' + swarm[agent_index] + '/ground_truth_to_tf/pose',
-                         PoseStamped, agent_callback, (swarm_pose, agent_index))
+	rospy.Subscriber('/' + rospy.myargv()[1] + '/ground_truth_to_tf/pose', PoseStamped, heading_allignment2)
 
-    # Publish the velocity of the agent
-    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
-    rate = rospy.Rate(10)  # 10Hz
+	pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
-    while not rospy.is_shutdown():
-        # Heading vector is one pillor of the swarm and here is initialized with a Twist() object
-        heading_vector = Twist()
+	rate = rospy.Rate(5)
 
-        #################################
-        ## heading alignment behaviour ##
-        #################################
+	while not rospy.is_shutdown():
+		msg = Twist()
+		heading_vector = Twist()
 
-        x_roll = 0  # angular motion in x-axix
-        y_pitch = 0  # angular motion in y-axix
+		if(len(allignment_data) != 0 and abs(allignment_data[0][1].pose.position.z - msg_pose.pose.position.z) > 1):
+			#rospy.loginfo(allignment_data[0][1].pose.position.z)
+			msg.linear.z = allignment_data[0][1].pose.position.z - msg_pose.pose.position.z
+			pub.publish(msg)
+			rate.sleep()
+			continue
 
-        for agent in swarm_pose:
-            # TODO: proofcheck
-            # the position vectors, of each agent in the swarm, of the form PoseStamped() containing position
-            # information of other agents in the swarm, corresponding direction angle in xy axis is found
-            # and accumulate to pitch and roll values
-            angle = quaternionToAngle(agent.pose.orientation)
-            x_roll += math.cos(angle)
-            y_pitch += math.sin(angle)
-            rospy.loginfo('angle:' + str(angle))
+		# heading alignment behaviour
+		numerator_x = 0
+		numerator_y = 0
+		denominator = 0
+		
+		for data in allignment_data:
+			q = data[1].pose.orientation
+			yaw = math.atan2(2 * (q.w * q.z + q.x * q.y), 1 - (2 * (q.y * q.y + q.z * q.z)))
+			#rospy.loginfo(str(math.atan2(2 * (q.w * q.z + q.x * q.y), 1 - (2 * (q.y * q.y + q.z * q.z)))))
+			numerator_x += math.cos(yaw)
+			numerator_y += math.sin(yaw)
 
-        # The norm is the hypothenuse of the right angle of roll, pitch and angle: update expalanation
-        norm = math.sqrt((x_roll * x_roll) + (y_pitch * y_pitch))
+		denominator = math.sqrt((numerator_x * numerator_x) + (numerator_y * numerator_y))
+		if denominator == 0:
+			denominator = 1
+		heading_vector.linear.x = numerator_x / denominator
+		heading_vector.linear.y = numerator_y / denominator
 
-        # initialize a heading_vector
-        if abs(norm) < epsion:  # epsilon is very close to zero
-            heading_vector.linear.x = 0
-            heading_vector.linear.y = 0
-        else:
-            heading_vector.linear.x = x_roll / norm
-            heading_vector.linear.y = y_pitch / norm
+		#rospy.loginfo(str(heading_vector.linear.x) + " " + str(heading_vector.linear.y))
 
-        ################################
-        ## proximal control behaviour ##
-        ################################
+		#proximal control behaviour
+		force = 0
+		position_vector = Twist()
+		for data in allignment_data:
+			distance = math.sqrt((data[1].pose.position.x - msg_pose.pose.position.x) * (data[1].pose.position.x - msg_pose.pose.position.x) + 
+				(data[1].pose.position.y - msg_pose.pose.position.y) * (data[1].pose.position.y - msg_pose.pose.position.y))
+			if distance > 2:
+				force = (2 * (2 - distance)) * (2 * (2 - distance)) / 5
+				position_vector.linear.x += msg_pose.pose.position.x * force
+				position_vector.linear.y += msg_pose.pose.position.y * force
+			else:
+				force = - (2 * (2 - distance)) * (2 * (2 - distance)) / 5
+				position_vector.linear.x += msg_pose.pose.position.x * force
+				position_vector.linear.y += msg_pose.pose.position.y * force
+			rospy.loginfo(str(force) + " " + str(distance))
+		#rospy.loginfo(str(position_vector.linear.x) + " " + str(position_vector.linear.y))
 
-        position_vector = Twist()
-        o_des = 1.75  # Constant in the formula
-        force = 0
-        C = 5  # Constant in the formula
+		#desired heading vector
+		a = Twist()
+		numerator_x = 0
+		numerator_y = 0
+		denominator = 0
 
-        for agent_pose in swarm_pose:
-            # for the current and each agent in the swarm, find the difference in between and in all three axes
-            d_x = agent_pose.pose.position.x - slave_pose.pose.position.x
-            d_y = agent_pose.pose.position.y - slave_pose.pose.position.y
-            d_z = agent_pose.pose.position.z - slave_pose.pose.position.z
+		numerator_x = heading_vector.linear.x + (12 * position_vector.linear.x)
+		numerator_y = heading_vector.linear.y + (12 * position_vector.linear.y)
 
-            # Find distance from the difference in three axes
-            distance = math.sqrt(d_x * d_x + d_y * d_y + d_z * d_z)
+		denominator = math.sqrt((numerator_x * numerator_x) + (numerator_y * numerator_y))
+		if denominator == 0:
+			denominator = 1
 
-            # If the distance, i.e. the agent, is far from how much it is supposed to be,
-            if distance > o_des:
-                # increment force f_k by ((o_des - o_k))^2 / C
-                force += ((o_des - distance) ** 2) / C
-            else:
-                # otherwise, decrement by ((o_des - o_k))^2 / C.
-                force -= ((o_des - distance) ** 2) / C
+		a.linear.x = numerator_x / denominator
+		a.linear.y = numerator_y / denominator
 
-        # update position vector with corresponding position vector
-        position_vector.linear.x = slave_pose.pose.position.x * force
-        position_vector.linear.y = slave_pose.pose.position.y * force
-        position_vector.linear.z = slave_pose.pose.position.z * force
-
-        ############################
-        ## desired heading vector ##
-        ############################
-
-        # Desired Heading Vector 'a' is initialized with a Twist() object
-        a = Twist()
-
-        # initializing the numerators in 3 axes of Desired Heading Vector and the denominator
-        numerator_x = 0
-        numerator_y = 0
-        numerator_z = 0
-        denominator = 0
-        Beta = 12  # Constant in the formula
-
-        # updating numerators with heading vector and proximal control vector in corresponding coodinate axis
-        numerator_x = heading_vector.linear.x + \
-            (Beta * position_vector.linear.x)
-        numerator_y = heading_vector.linear.y + \
-            (Beta * position_vector.linear.y)
-        numerator_z = heading_vector.linear.z + \
-            (Beta * position_vector.linear.z)
-
-        # updating denominator with the norm of numerators
-        denominator = math.sqrt((numerator_x * numerator_x) +
-                                (numerator_y * numerator_y) + (numerator_z * numerator_z))
-
-        if abs(denominator) < epsion:  # epsilon is very close to zero
-            # if denominator is zero, then the numerators in all axes are all zero
-            a.linear.x = 0
-            a.linear.y = 0
-            a.linear.z = 0
-        else:
-            a.linear.x = numerator_x / denominator
-            a.linear.y = numerator_y / denominator
-            a.linear.z = numerator_z / denominator
-
-        ####################
-        ## motion control ##
-        ####################
-
-        dot_product = (a.linear.x * slave_pose.pose.position.x) + \
-            (a.linear.y * slave_pose.pose.position.y)
-        if dot_product >= 0:
-            slave_vel.linear.y = (dot_product ** 1) * 7
-        else:
-            slave_vel.linear.y = 0
-
-        if math.sqrt(a.linear.x ** 2 + a.linear.y ** 2) * math.sqrt(slave_pose.pose.position.x ** 2 + slave_pose.pose.position.y ** 2) != 0:
-            slave_vel.angular.z = math.acos(dot_product / (math.sqrt(a.linear.x ** 2 + a.linear.y ** 2)
-                                                           * math.sqrt(slave_pose.pose.position.x ** 2 + slave_pose.pose.position.y ** 2))) * 0.5
-
-        rospy.loginfo_throttle(4.0, 'heading ' + TwistToStr(heading_vector))
-        rospy.loginfo_throttle(4.0, 'position ' + TwistToStr(position_vector))
-        rospy.loginfo_throttle(4.0, 'velocity ' + TwistToStr(slave_vel))
-        pub.publish(slave_vel)
-        rate.sleep()
+		#motion control
+		q = msg_pose.pose.orientation
+		yaw_current = math.atan2(2 * (q.w * q.z + q.x * q.y), 1 - (2 * (q.y * q.y + q.z * q.z)))
+		
+		dot_product = (a.linear.x * math.cos(yaw_current)) + (a.linear.y * math.sin(yaw_current))
+		if dot_product >= 0:
+			msg.linear.x = (dot_product ** 1) * 7
+		else:
+			msg.linear.x = 0
+		if math.sqrt(a.linear.x ** 2 + a.linear.y ** 2) * math.sqrt(math.cos(yaw_current) ** 2 + math.sin(yaw_current) ** 2) != 0:
+			if((a.linear.x < 0 and a.linear.y < 0) or (a.linear.x > 0 and a.linear.y < 0)):
+				msg.angular.z = - math.acos(dot_product / (math.sqrt(a.linear.x ** 2 + a.linear.y ** 2) * math.sqrt(math.cos(yaw_current) ** 2 + math.sin(yaw_current) ** 2))) * 0.5
+			else:
+				msg.angular.z = math.acos(dot_product / (math.sqrt(a.linear.x ** 2 + a.linear.y ** 2) * math.sqrt(math.cos(yaw_current) ** 2 + math.sin(yaw_current) ** 2))) * 0.5
+		#rospy.loginfo(str(math.acos(dot_product / (math.sqrt(a.linear.x ** 2 + a.linear.y ** 2) * math.sqrt(msg_pose.pose.position.x ** 2 + msg_pose.pose.position.y ** 2)))) + "!")
+		#rospy.loginfo(str(a))
+		#rospy.loginfo((str(msg.angular.z)))
+		#rospy.loginfo(str(math.cos(yaw_current)) + " " + str(math.sin(yaw_current)))
+		#rospy.loginfo(str(msg.linear.x) + " " + str(msg.linear.y) + " " + str(msg.linear.z) + " " + str(msg.angular.x) + " " + str(msg.angular.y) + " " + str(msg.angular.z))  
+		pub.publish(msg)
+		rate.sleep()
